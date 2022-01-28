701816d94e92b3c007894164df2859f0f456d879
James McKenna <james@systemsdesign.rocks>
2022-01-28 12:23:51 -0700

    Added 404 not found page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>404 Page Not Found</h2>
+  <h3>&#128542; Sorry, our helper elves weren't able to find that page.</h3>
+</template>
+
+<style scoped>
+h3 {
+ text-decoration: none;
+ margin: 1rem 1rem;
+}
+</style>
