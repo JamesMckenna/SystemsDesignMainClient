75209d653a5edf2572d2c3ea8fa11c4667f3ede4
James McKenna <james@systemsdesign.rocks>
2022-03-18 13:02:34 -0600

    Upgraded to vue/cli 5, fixed dependency tree, set up tooling

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { store } from './store'
-import '@/assets/styles/reset.module.css'
+import '@/assets/styles/reset.css'
 import '@/assets/styles/normalize.css'
-// import "@/assets/styles/site.module.css"
+//import "@/assets/styles/site.css"
 import '@/assets/styles/nav.css'
 
 createApp(App).use(store).use(router).mount('#app')
