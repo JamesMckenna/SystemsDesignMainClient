293cebdec357fa3de3c791d880f053afa288d25e
James McKenna <james@systemsdesign.rocks>
2022-03-22 15:30:20 -0600

    Testing build tooling, un-commenting plugin code to see where git commit hook and  linting error

--- vue.config.js
+++ vue.config.js
@@ -2,38 +2,8 @@ var TerserPlugin = require("terser-webpack-plugin");
 var WebpackObfuscator = require("webpack-obfuscator");
 var path = require("path");
 
-//https://webpack.js.org/plugins/terser-webpack-plugin/
-// const minify = (input, sourceMap, minimizerOptions, extractsComments) => {
-//   // The `minimizerOptions` option contains option from the `terserOptions` option
-//   // You can use `minimizerOptions.myCustomOption`
-
-//   // Custom logic for extract comments
-//   const { map, code } = require("uglify-module") // Or require('./path/to/uglify-module')
-//     .minify(input, {
-//       /* Your options for minification */
-//     });
-
-//   return { map, code, warnings: [], errors: [], extractedComments: [] };
-// };
-
-// Used to regenerate `fullhash`/`chunkhash` between different implementation
-// Example: you fix a bug in custom minimizer/custom function, but unfortunately webpack doesn't know about it, so you will get the same fullhash/chunkhash
-// to avoid this you can provide version of your custom minimizer
-// You don't need if you use only `contenthash`
-// minify.getMinimizerVersion = () => {
-//   let packageJson;
-
-//   try {
-//     // eslint-disable-next-line global-require, import/no-extraneous-dependencies
-//     packageJson = require("uglify-module/package.json");
-//   } catch (error) {
-//     // Ignore
-//   }
-
-//   return packageJson && packageJson.version;
-// };
 const terserOptionsCompress = {
-  defaults: true, //keep defaults unless other explicitly changed in options below
+  defaults: true, //keep defaults unless otherwise explicitly changed in options.property below
   booleans_as_integers: true, // (default: false) -- Turn booleans into 0 and 1, also makes comparisons with booleans use == and != instead of === and !==.
   drop_console: true, // (default: false) -- Pass true to discard calls to console.* functions. If you wish to drop a specific function call such as console.info and/or retain side effects from function arguments after dropping the function call then use pure_funcs instead.
   ecma: "ESNext", // (default: 5) -- Pass 2015 or greater to enable compress options that will transform ES5 code into smaller ES6+ equivalent forms.
@@ -72,32 +42,18 @@ module.exports = {
     optimization: {
       minimize: true,
       minimizer: [
-        new TerserPlugin(
-          {
-            test: /\.js(\?.*)?$/i,
-            include: "./src/*",
-            exclude: [path.resolve(__dirname, "node_modules")],
-            extractComments: true,
-            minify: TerserPlugin.terserMinify,
-            terserOptions: {
-              ecma: "ESNext",
-              //parse: {},
-              compress: terserOptionsCompress,
-              mangle: true, // Note `mangle.properties` is `false` by default.
-              module: false,
-              // Deprecated
-              //output: null,
-              //format: null,
-              //toplevel: false,
-              //nameCache: null,
-              //ie8: false,
-              //keep_classnames: undefined,
-              //keep_fnames: false,
-              //safari10: false,
-            },
-          }
-          //minify,
-        ),
+        new TerserPlugin({
+          test: /\.js(\?.*)?$/i,
+          include: "./src/*",
+          exclude: [path.resolve(__dirname, "node_modules")],
+          extractComments: true,
+          minify: TerserPlugin.terserMinify,
+          terserOptions: {
+            ecma: "ESNext",
+            compress: terserOptionsCompress,
+            mangle: true, // Note `mangle.properties` is `false` by default.
+          },
+        }),
       ],
     },
     module: {
