7f85f6623b4eb4510dd35710c61f34e47966ae9d
James Mckenna <G.James.Mckenna@hotmail.com>
2022-04-17 13:36:53 -0600

    Added components for 3d assets

---
+++ src/components/babylon/gears.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="body-container">
+    <h2>Animated 3D Worm Gear and Worm Wheels</h2>
+    <canvas id="render-canvas"></canvas>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from "vue";
+import { Engine, Scene, SceneLoader, Color4 } from "babylonjs";
+let canvas: HTMLCanvasElement;
+let engine: Engine;
+let scene: Scene;
+
+onMounted(() => {
+  canvas = document.getElementById("render-canvas")! as HTMLCanvasElement;
+  engine = new Engine(canvas, true);
+  scene = new Scene(engine);
+
+  SceneLoader.Append("", "gears/gears.babylon", scene);
+  scene.executeWhenReady(() => {
+    scene.clearColor = new Color4(0, 0, 0, 0.0000001);
+    scene.activeCamera!.attachControl(canvas);
+
+    engine.runRenderLoop(() => {
+      scene.render();
+    });
+  });
+
+  window.addEventListener("resize", () => {
+    engine.resize();
+  });
+});
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 1rem;
+}
+canvas {
+  width: 100%;
+  height: 90vh;
+}
+.body-container {
+  margin-top: 6rem;
+  width: 100%;
+  height: 100vh;
+}
+</style>
